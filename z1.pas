ce997f3c9df07fc059f98b78b1851d6e9f89264a
Tesak-NEtGlaZ <lvm07kirov@gmail.com>
2025-04-14 17:17:00 +0300

    Add files via upload

---
+++ z1.pas
@@ -0,0 +1,19 @@
+﻿program PointerExample;
+
+var
+  i: integer;
+  i_ptr: ^integer;
+
+begin
+  i := 2;
+
+  i_ptr := @i;
+
+  writeln('Значение переменной i: ', i);
+  writeln('Значение по адресу i_ptr: ', i_ptr^);
+
+  i_ptr^ := 5;
+  writeln('После изменения через указатель:');
+  writeln('Значение переменной i: ', i);
+  writeln('Значение по адресу i_ptr: ', i_ptr^);
+end.
